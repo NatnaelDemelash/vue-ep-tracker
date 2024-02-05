3f6401922c693eacfd9094a74df516b9a5717df2
Natnael Demelash <natidemelash074@gmail.com>
2024-02-05 20:32:38 +0300

    UI done for the whole page

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3>Add New Transaction</h3>
+  <form id="form">
+    <div class="form-control">
+      <label for="text">Text</label>
+      <input type="text" id="text" placeholder="Enter text ...">
+    </div>
+    <div class="form-control">
+      <label for="amount">Amount</label>
+      <input type="number" id="amount" placeholder="Enter amount ....">
+    </div>
+    <button class="btn">Add Transaction</button>
+  </form>
+</template>
+
---
+++ src/components/TransactionHistory.vue
@@ -0,0 +1,12 @@
+<template>
+ <h3>History</h3>
+  <ul class="list" id="list">
+    <li class="minus">
+      Cash <span>-$400</span> <button class="delete-btn">X</button>
+    </li>
+    <li class="plus">
+      Pay Check <span>+$500</span> <button class="delete-btn">X</button>
+    </li>
+  </ul>
+</template>
+
